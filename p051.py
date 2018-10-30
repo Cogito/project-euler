ba9ccf4af34a10e262265a453be86c4a8f5300e8
Andrew Ardill <andrew.ardill@gmail.com>
2018-10-30 22:43:16 +1100

    Attempted solution for p051
    Too slow, as it goes through all numbers one by one
    trying adding digits and seeing if they're prime.
    
    Instead should go through primes and try replacing
    characters.

--- p051.py
+++ p051.py
@@ -1,23 +1,24 @@
-from itertools import combinations
-from euler_helpers import printlist
+from itertools import combinations, cycle, count
+from euler_helpers import printlist, is_prime
 
 
 def insert_digits(number, num_digits):
-    length = len(str(number))
-    combos = combinations(range(length), length - num_digits)
+    length = len(str(number)) + num_digits
     output = []
-    for digit in range(num_digits):
-        output.append((['*' if i in combo else 1 for i in range(3)] for combo in combos))
+    char_cyc = cycle(str(number))
+    combos = combinations(range(length), num_digits)
+    for combo in combos:
+        output.append([int("".join([str(digit) if i in combo else next(char_cyc) for i in range(length)])) for digit in range(10)])
+    return output
 
 
 def p051():
-    length = 2
-    # positions = itertools.permutations(integers(end=length), 2)
-
-    # printlist(integers(end=length))
-    # printlist(positions)
-    combos = combinations(range(3), 1)
-    printlist()
+    for number in count():
+        for family in insert_digits(number, 2):
+            if sum(1 if is_prime(n) else 0 for n in family) >= 8:
+                print([1 if is_prime(n) else 0 for n in family])
+                print(family)
+                return
 
 
 if __name__ == '__main__':
