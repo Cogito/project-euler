239abd0094469dbfb81467dd61bb17ca671fa9fe
Andrew Ardill <andrew.ardill@gmail.com>
2018-10-31 09:18:26 +1100

    Attempt p051 by iterating through the primes
    Still seems slow, I think because I am only checking
    doubles, not triples or higher.

--- p051.py
+++ p051.py
@@ -1,5 +1,6 @@
 from itertools import combinations, cycle, count
-from euler_helpers import printlist, is_prime
+from euler_helpers import printlist, is_prime, primes
+from tqdm import tqdm
 
 
 def insert_digits(number, num_digits):
@@ -12,14 +13,40 @@ def insert_digits(number, num_digits):
     return output
 
 
+def replace_digits(number, positions):
+    number_string = str(number)
+    output = []
+    for position in positions:
+        output.append([int("".join([str(digit) if i in position else number_string[i] for i in range(len(number_string))])) for digit in range(10)])
+    return output
+
+
+def find_doubles(number):
+    locations = [[], [], []]
+    for i, c in enumerate(str(number)):
+        if c == '0':
+            locations[0].append(i)
+        if c == '1':
+            locations[1].append(i)
+        if c == '2':
+            locations[2].append(i)
+    doubles = []
+    for l in locations:
+        if len(l) >= 2:
+            doubles.extend(combinations(l, 2))
+    return doubles
+
+
 def p051():
-    for number in count():
-        for family in insert_digits(number, 2):
-            if sum(1 if is_prime(n) else 0 for n in family) >= 8:
+    for number in tqdm(primes()):
+        for family in replace_digits(number, find_doubles(number)):
+            if sum(1 if is_prime(n) else 0 for n in family) >= 7:
                 print([1 if is_prime(n) else 0 for n in family])
                 print(family)
-                return
+                if sum(1 if is_prime(n) else 0 for n in family) >= 8:
+                    return
 
 
 if __name__ == '__main__':
     p051()
+    # printlist(list(d) for d in replace_digits(1234515812, find_doubles(1234515812)))
